a5071b68c2b7b93bf37dbf17f57e117d78ce4fc3
Lei Xu <leixu@leixu-ubu-sig-vm102>
2025-11-02 19:13:18 +0000

    初始化 qa-service-user 微服务项目

---
+++ server/qa-service-user/src/main/java/com/leansofx/qaserviceuser/QaserviceuserApplication.java
@@ -0,0 +1,13 @@
+package com.leansofx.qaserviceuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaserviceuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QaserviceuserApplication.class, args);
+	}
+
+}
